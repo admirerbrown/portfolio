17d8acad8c729f93b2c388610e35783c42817828
admirerbrown <admirerbrown@gmail.com>
2022-11-17 18:24:37 +0000

    peer programming collab

--- validate_form.js
+++ validate_form.js
@@ -1,11 +1,12 @@
+const email = document.querySelector('#email');
+const form = document.querySelector('#form');
+const errorMsg = document.querySelector('.error-msg');
+
 form.addEventListener('submit', (event) => {
-    const usermail = email.value;
-    if (/[A-Z]/.test(usermail)) {
-      errorMsg.innerHTML = 'failed: email must be in lowercase.eg(johndoe@gmail.com)';
-      errorMsg.classList.add('invalid');
-      event.preventDefault();
-    }
-  const email = document.querySelector('#email');
-  const form = document.querySelector('#form');
-  const errorMsg = document.querySelector('.error-msg');
+  const usermail = email.value;
+  if (/[A-Z]/.test(usermail)) {
+    errorMsg.innerHTML = 'failed: email must be in lowercase.eg(johndoe@gmail.com)';
+    errorMsg.classList.add('invalid');
+    event.preventDefault();
+  }
 });
