0704fe009e561e95c438b77692921032eccfdfb0
admirerbrown <admirerbrown@gmail.com>
2022-11-19 11:55:41 +0000

    fix live site bug

--- validate_form.js
+++ validate_form.js
@@ -24,6 +24,8 @@ form.addEventListener('input', () => {
 
 // pre fill user form details from localStorage
 const getUserFormInput = JSON.parse(localStorage.getItem('formData'));
-document.querySelector('#fname').value = getUserFormInput.fname;
-document.querySelector('#email').value = getUserFormInput.email;
-document.querySelector('#comments').value = getUserFormInput.comment;
+if (getUserFormInput != null) {
+  document.querySelector('#fname').value = getUserFormInput.fname;
+  document.querySelector('#email').value = getUserFormInput.email;
+  document.querySelector('#comments').value = getUserFormInput.comment;
+}
